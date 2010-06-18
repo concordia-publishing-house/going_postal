ab1154610885f2c4c2c24a173db6454717b5d616
Jamin Leopold <jamin.leopold@cph.org>
2010-06-18 15:13:05 -0500

    Finished household and people mapping for multiple addresses, along with clickable map marker.

--- assets/javascripts/address_gapi.js
+++ assets/javascripts/address_gapi.js
@@ -1,6 +1,20 @@
+// date: 2010-06-18
+// author: Jamin Leopold
+
 /******************************************
  *    Base functions for use in code      *
  ******************************************/
+
+// function importJS(path){ 
+//   var script = document.createElement('script');
+//   script.type = 'text/javascript'; 
+//   script.src = path;
+//   document.getElementsByTagName('head')[0].appendChild(script); 
+// 
+//   // var i, base, src = "grid.js", scripts = document.getElementsByTagName("script"); 
+//   // for (i=0; i<scripts.length; i++){if (scripts[i].src.match(src)){ base = scripts[i].src.replace(src, "");break;}}
+//   // document.write("<" + "script src=\"" + base + path + "\"></" + "script>"); 
+// } 
 
 var displayDebug = true; //TODO: turn off for production!
 function debug(msg) {
@@ -9,22 +23,13 @@ function debug(msg) {
   //else { alert(msg) }
 }
 
-//dumpProps uses 'debug' method
-function dumpProps(obj, parent) {
-  // Go through all the properties of the passed-in object
-  for (var i in obj) {
-    // if a parent (2nd parameter) was passed in, then use that to
-    // build the message. Message includes i (the object's property name)
-    // then the object's property value on a new line
-    if (parent) {var msg = parent + "." + i + "\n" + obj[i];} else {var msg = i + "\n" + obj[i];}
-    // Display the message. If the user clicks "OK", then continue. If they
-    // click "CANCEL" then quit this level of recursion
-    if (!debug(msg)) {return;}
-    // If this property (i) is an object, then recursively process the object
-    if (typeof obj[i] == "object") {
-      if (parent) {dumpProps(obj[i], parent + "." + i);} else {dumpProps(obj[i], i);}
+//displayProps uses 'debug' method
+function displayProps(obj) {
+  var str ="Object properties:\n";
+  for(prop in obj) {
+    str+=prop + " value :"+ obj[prop]+"\n";//Concatenate prop and its value from object
     }
-  }
+  debug(str);
 }
 
 
@@ -33,10 +38,15 @@ function dumpProps(obj, parent) {
  *   Map API using Google Maps Geocoder   *
  *                                        *
  ******************************************/
+
+ // Load google maps api with key here at start!
+ // importJS('http://maps.google.com/maps/api/js?sensor=false&key=ABQIAAAASklC1xi8fElpu98ovSmxkBS2nENSybmWYSq8MgbOY85MooMpuxSuV-mV3BvfBWM7ag5kx1HB9CSPiQ'); //for church360.org
+ // importJS('http://maps.google.com/maps/api/js?sensor=false&key=ABQIAAAAnfs7bKE82qgb3Zc2YyS-oBT2yXp_ZAY8_ufC3CFXhHIE1NvwkxSySz_REpPq-4WZA27OwgbtyR3VcA'); //for localhost
+
 var geocoder;
 var map;
 var latlng = new google.maps.LatLng(-43.53262,172.63504); // IF no address in form, it starts here (City: Christchurch, Country: New Zealand).
-// var latlng = new google.maps.LatLng(0,0); // IF no address in form, it starts here (mid-ocean blue).
+var latlng = new google.maps.LatLng(0,0); // IF no address in form, it starts here (mid-ocean blue).
 var myOptions = {
   center: latlng,
   mapTypeId: google.maps.MapTypeId.ROADMAP,
@@ -53,28 +63,19 @@ var myOptions = {
 function map_initialize(mapTypeID) {
   if (mapTypeID) { 
     geocoder = new google.maps.Geocoder(); 
+    // mapTypeID = new google.maps.Map(document.getElementById(mapTypeID+'_address'), myOptions); //Create map object using a variable's contents as name of object gives "not defined"
     switch(mapTypeID) {
-      case 'map_perm':
-        map_perm = new google.maps.Map(document.getElementById(mapTypeID+'_address'), myOptions);
-        break;
-      case 'map_mail':
-        map_mail = new google.maps.Map(document.getElementById(mapTypeID+'_address'), myOptions);
-        break;
-      case 'map_away':
-        map_away = new google.maps.Map(document.getElementById(mapTypeID+'_address'), myOptions);
-        break;
+      case 'map_perm': map_perm = new google.maps.Map(document.getElementById(mapTypeID+'_address'), myOptions); break;
+      case 'map_mail': map_mail = new google.maps.Map(document.getElementById(mapTypeID+'_address'), myOptions); break;
+      case 'map_away': map_away = new google.maps.Map(document.getElementById(mapTypeID+'_address'), myOptions); break;
     }
   } else {
-    debug("!No mapTypeID passed as argument to map_initialize!")
+    debug("!No map ID passed as argument to map_initialize!")
   }
 }
 
 function mapAddress(addressSource, mapName, init) {
-  if(!init) {init=false;}
   addressSource ? address = concatAddress(addressSource) : address = 'unknown';
-  debug("mapAddressSource: " + addressSource );
-  if(!init) { var check = checkAddress(address); }
-
   if (geocoder) {
     geocoder.geocode( {
       'address': address
@@ -87,16 +88,31 @@ function mapAddress(addressSource, mapName, init) {
           map: mapName,
           title: 'Click to open map of this address.',
            //shape: (coords=[60,50,10,15], type='rect'),
-          position: results[0].geometry.location,
+          position: mapName.center,
           icon: '/javascripts/going_postal/home_icon_small.png',
           clickable: true
         });
+        var infowindow = infoAddress(mapName);
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.open(mapName,marker);
+        });
       } else {
-        debug("Geocode was not successful with "+ addressSource +" for the following status reason: " + status); //TODO: remove from debug, send message to end user.
-        //        alert("Geocode was not successful with "+ addressSource +" for the following status reason: " + status);
+        debug("Geocode was not successful with "+ addressSource +" for the following status reason: " + status); 
+        displayProps(mapName.c.id);
+        document.getElementById(mapName.c.id).style.display="none";
       }
     });
   }
+}
+
+function infoAddress(mapName) {
+  var infoVar = new google.maps.InfoWindow({ 
+    content: "<a href='http://maps.google.com/maps?q="+mapName.center+"' target='_blank'>Click to open large<br/>map of this address.</a>",
+    position: mapName.center,
+    maxWidth: 50,
+    size: new google.maps.Size(50,50)
+  });
+  return infoVar;
 }
 
 function checkAddress(addressSource) {
@@ -108,7 +124,7 @@ function checkAddress(addressSource) {
     if (status == google.maps.GeocoderStatus.OK) {
       debug("-->GeocoderStatus OK");
       if (results.length > 1) {
-        alert("We found more than one address with your '"+ addressSource +"', please be more specific.");
+        alert("We found more than one address with your '"+ addressSource +"', please be more specific.");  // this doesn't ever run...
         debug("-->checkAddress found multiple addresses:");
         debug(results);
         return 2; //multiple
