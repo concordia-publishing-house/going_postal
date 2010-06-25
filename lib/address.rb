6c7c23378dce7e610bf1d1c396fd5a27192de27e
Robert Lail <lailrc@a09-0016.cph.pri>
2010-06-25 09:09:52 -0500

    updated address class

--- lib/address.rb
+++ lib/address.rb
@@ -1,66 +1,8 @@
-require 'composite/base'
-
 class Address < Composite::Base
   include HashAccessors
-  hash_accessors_for :hash, [:street, :city, :state, :zip]
-
-  def initialize(hash=nil)
-    super()
-    @hash = hash.is_a?(Hash) ? symbolize_keys(hash) : {}
-  end
+  attr_reader :hash
+  hash_readers_for :hash, [:street, :city, :state, :zip]
   
-  # Convert all of the keys in the hash to symbols. This is taken from Rails hash extensions.
-  # It is necessary to create all addresses the same as searches against the address is a text
-  # search with the YAML from an address.
-  def symbolize_keys(hash)
-    return hash if not hash
-    new_hash = {}
-    hash.each do |k,v|
-      new_hash[k.to_sym] = v
-    end
-    new_hash
-  end
-  
-  def to_s
-    if (street and (street.length>0))
-      "#{street}\r\n#{city}, #{state} #{zip}"
-    end
-  end
-  
-  def to_html
-    # todo 'h' is not recognized; but add html cleaning here
-    html = "#{street}"
-    html << "<br/>" if(street and (street.length>0))
-    html << "#{city}, #{state} #{zip}" if(city and (city.length>0))
-    html
-  end
-  
-  def self.loader(address_as_string)
-    if address_as_string
-      Address.new YAML::load(address_as_string)
-    else
-      Address.new
-    end
-  end
-  
-  def self.converter(value)
-    return Address.new(value) if value.is_a? Hash
-    return nil
-  end
-  
-  def to_yaml
-    # calling to_hash first will make sure the hash is a Hash and not HashWithIndifferentAccess
-    symbolize_keys(@hash).to_hash.to_yaml
-  end
-  
-  def blank?
-    (street.blank? and city.blank? and state.blank? and zip.blank?)
-  end
-
-  def exists?
-    !blank?
-  end
-
   STATES = [
 #    [ "Select a State", "" ],
     [ "Alabama", "AL" ],
@@ -114,23 +56,142 @@ class Address < Composite::Base
     [ "West Virginia", "WV" ],
     [ "Wisconsin", "WI" ],
     [ "Wyoming", "WY" ]
-  ]
+  ]  
 
-  protected
+  STATES_ABBREVIATED = [
+    [ "AL" ],
+    [ "AK" ],
+    [ "AZ" ],
+    [ "AR" ],
+    [ "CA" ],
+    [ "CO" ],
+    [ "CT" ],
+    [ "DE" ],
+    [ "FL" ],
+    [ "GA" ],
+    [ "HI" ],
+    [ "ID" ],
+    [ "IL" ],
+    [ "IN" ],
+    [ "IA" ],
+    [ "KS" ],
+    [ "KY" ],
+    [ "LA" ],
+    [ "ME" ],
+    [ "MD" ],
+    [ "MA" ],
+    [ "MI" ],
+    [ "MN" ],
+    [ "MS" ],
+    [ "MO" ],
+    [ "MT" ],
+    [ "NE" ],
+    [ "NV" ],
+    [ "NH" ],
+    [ "NJ" ],
+    [ "NM" ],
+    [ "NY" ],
+    [ "NC" ],
+    [ "ND" ],
+    [ "OH" ],
+    [ "OK" ],
+    [ "OR" ],
+    [ "PA" ],
+    [ "PR" ],
+    [ "RI" ],
+    [ "SC" ],
+    [ "SD" ],
+    [ "TN" ],
+    [ "TX" ],
+    [ "UT" ],
+    [ "VT" ],
+    [ "VA" ],
+    [ "WA" ],
+    [ "WV" ],
+    [ "WI" ],
+    [ "WY" ]
+  ]  
 
-  def validate
-#    errors.add( "street", "must contain a street" ) if ( street.length == 0)
-#    errors.add( "city", "must contain a city" ) if ( city.length == 0)
-#    errors.add( "state", "must contain a state" ) if ( state.length == 0)
-#    errors.add( "zip", "must contain a zip" ) if ( zip.length == 0)
 
-    errors.add_to_base( "must contain a street." ) if ( street.blank?)
-    errors.add_to_base( "must contain a city." ) if ( city.blank?)
-    errors.add_to_base( "must contain a state." ) if ( state.blank?)
-    errors.add_to_base( "must contain a zip." ) if ( zip.blank?)
-
-    errors.add( "zip", "is the wrong format (<em>xxxxx</em> or <em>xxxxx-xxxx</em>)." ) unless ( zip.match(/\A\d{5}(-\d{4})?\Z/) or zip.blank?)
-#    errors.add( "state", "should be a 2-letter abbreviation." ) unless ( (:state =~ /\A[\w]{2}\Z/ ) or state.length==0 )  #user will choose from select field
+  def initialize(hash=nil)
+    super()
+    @hash = hash.is_a?(Hash) ? symbolize_keys(hash) : {}
+    @hash.reverse_merge!(
+      :street => "",
+      :city => "",
+      :state => "",
+      :zip => "")
+   end
+  
+  # Convert all of the keys in the hash to symbols. This is taken from Rails hash extensions.
+  # It is necessary to create all addresses the same as searches against the address is a text
+  # search with the YAML from an address.
+  def symbolize_keys(hash)
+    return hash if not hash
+    new_hash = {}
+    hash.each do |k,v|
+      new_hash[k.to_sym] = v
+    end
+    new_hash
+  end
+  
+  def ==(other)
+    other.is_a?(Address) ? (
+      (self.street == other.street) &&
+      (self.city == other.city) &&
+      (self.state == other.state) &&
+      (self.zip == other.zip)
+    ) : false
   end
 
+  def to_s
+    if (street and (street.length>0))
+      "#{street}\n#{city}, #{state}  #{zip}"
+    end
+  end
+  
+  def to_html
+    # todo 'h' is not recognized; but add html cleaning here
+    html = "#{street}"
+    html << "<br/>" if(street and (street.length>0))
+    html << "#{city}, #{state} #{zip}" if(city and (city.length>0))
+    html
+  end
+  
+  def self.loader(address_as_string)
+    address_as_string ? Address.new(YAML::load(address_as_string)) : Address.new
+  end
+  
+  def self.converter(value)
+    value.is_a?(Hash) ? Address.new(value) : nil
+  end
+  
+  def to_hash
+    # calling to_hash first will make sure the hash is a Hash and not HashWithIndifferentAccess
+    symbolize_keys(@hash).to_hash
+  end
+  
+  def to_yaml
+    to_hash.to_yaml
+  end
+
+  def blank?
+    (street.blank? and city.blank? and zip.blank?) # not "and state.blank?" because state can sometimes be set to a default
+  end
+
+  def exists?
+    !blank?
+  end
+
+protected
+
+  def validate
+    errors.add(:street, "cannot be blank") if street.blank?
+    errors.add(:city, "cannot be blank") if city.blank?
+    errors.add(:state, "cannot be blank") if state.blank?
+    errors.add(:zip, "cannot be blank") if zip.blank?
+    errors.add(:zip, "is the wrong format (<em>xxxxx</em> or <em>xxxxx-xxxx</em>).") if !zip.to_s.blank? and !zip.to_s.match(/\A\d{5}(-\d{4})?\Z/)
+    #errors.add(:state, "should be a 2-letter abbreviation." ) unless ( (:state =~ /\A[\w]{2}\Z/ ) or state.length==0 )  #user will choose from select field
+  end
+  
 end
