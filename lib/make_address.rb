6074f2ad8e152bd03d4db72cf102280746decfcd
Robert Lail <lailrc@a09-0016.cph.pri>
2010-06-28 08:41:59 -0500

    added make_address macro to plugin

---
+++ lib/make_address.rb
@@ -0,0 +1,14 @@
+module MakeAddress
+  
+  def make_address(*parts)
+    for part_id in parts
+      composed_of part_id,
+                  :class_name => "Address",
+                  :allow_nil => true,
+                  :mapping => [part_id, "to_yaml"], # changed because 'saver' is not a method of NilClass and this is an accurate name
+                  :constructor => :loader,
+                  :converter => :converter
+    end
+  end
+  
+end
