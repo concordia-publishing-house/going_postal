ceabf08d7d09a0a96f538bc00537921fcbcf995e
Robert Lail <lailrc@a09-0016.cph.pri>
2010-10-29 08:09:22 -0500

    clean up bad code related to errors

--- lib/active_model.rb
+++ lib/active_model.rb
@@ -39,16 +39,16 @@ module ActiveModel
     def any?
       !self.empty?
     end
-
+    
     def to_a
       @hash.inject([]) do |errors_with_attributes, (attribute, errors)|
-        if error.blank?
+        if errors.blank?
           errors_with_attributes
         else
-          if attr == :base
-            errors_with_attributes << error
+          if attribute == :base
+            errors_with_attributes.concat(errors)
           else
-            errors_with_attributes << (attribute.to_s.humanize + " " + error)
+            errors_with_attributes.concat(errors.map {|error| (attribute.to_s.humanize + " " + error)})
           end
         end
       end
@@ -58,42 +58,42 @@ module ActiveModel
       # ActiveSupport::Deprecation.warn "Errors#on have been deprecated, use Errors#[] instead"
       self[attribute]
     end
-
+    
     def on_base
       # ActiveSupport::Deprecation.warn "Errors#on_base have been deprecated, use Errors#[:base] instead"
       on(:base)
     end
-
+    
     def add(attribute, msg = Errors.default_error_messages[:invalid])
       # ActiveSupport::Deprecation.warn "Errors#add(attribute, msg) has been deprecated, use Errors#[attribute] << msg instead"
       self[attribute] << msg
     end
-
+    
     def add_to_base(msg)
       # ActiveSupport::Deprecation.warn "Errors#add_to_base(msg) has been deprecated, use Errors#[:base] << msg instead"
       self[:base] << msg
     end
-  
+    
     def invalid?(attribute)
       # ActiveSupport::Deprecation.warn "Errors#invalid?(attribute) has been deprecated, use Errors#[attribute].any? instead"
       self[attribute].any?
     end
-
+    
     def full_messages
       # ActiveSupport::Deprecation.warn "Errors#full_messages has been deprecated, use Errors#to_a instead"
       to_a
     end
-
+    
     def each_full
       # ActiveSupport::Deprecation.warn "Errors#each_full has been deprecated, use Errors#to_a.each instead"
       to_a.each { |error| yield error }
     end    
-
+    
     def to_xml(options={})
       options[:root]    ||= "errors"
       options[:indent]  ||= 2
       options[:builder] ||= Builder::XmlMarkup.new(:indent => options[:indent])
-
+      
       options[:builder].instruct! unless options.delete(:skip_instruct)
       options[:builder].errors do |e|
         to_a.each { |error| e.error(error) }
