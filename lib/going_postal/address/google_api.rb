7c72e97b2cbe01e9c471be95fcb2cb17425d9726
Robert Lail <lailrc@a09-0016.cph.pri>
2011-01-04 13:44:19 -0600

    added documentation to google_api.rb

--- lib/going_postal/address/google_api.rb
+++ lib/going_postal/address/google_api.rb
@@ -2,6 +2,24 @@ require 'net/http'
 require 'rack'
 
 
+# ===============================================================================
+# GoingPostal::Address::GoogleApi
+# ===============================================================================
+# 
+#   This module relies on Google Maps API, version 3
+#   http://code.google.com/apis/maps/documentation/geocoding/
+#
+#   It is important to note that Google enforces Usage Limits on this API
+#   by IP address of the computer making the request. This means that
+#   application logic that relies on server-side requests made to the API
+#   will more quickly exceed the request limit than application logic that
+#   moves API calls to the client.
+# 
+#  Google's Terms of Usage require that this API be used only in conjunction
+#  with Google Maps or Google Earth.
+#
+# ===============================================================================
+
 module GoingPostal
   class Address
     module GoogleApi
