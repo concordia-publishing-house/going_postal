c686d233956bb1eadda8993e59691eb0a0d5bb97
Luke Booth <Luke.Booth@cph.org>
2013-04-10 16:43:22 -0500

    [fix] parse YAML before doing a check for nil

--- lib/going_postal/make_address.rb
+++ lib/going_postal/make_address.rb
@@ -16,16 +16,17 @@ module GoingPostal
           class_eval <<-RUBY
             def #{part_id}
               attributes = super
+              attributes = YAML::load(attributes) if attributes.is_a?(String)
               return GoingPostal::Address.new if attributes.nil?
               
-              attributes = YAML::load(attributes) if attributes.is_a?(String)
-              GoingPostal::Address.new(attributes.merge(latitude: #{part_id}_latitude, longitude: #{part_id}_longitude))
+              GoingPostal::Address.new(attributes.merge(latitude: #{part_id}_latitude, longitude: #{part_id}_longitude)) # <-- difference: uses lat/long attrs
             end
             
             def #{part_id}=(address)
               address = GoingPostal::Address.new(address) if address.is_a?(Hash)
               super(address.blank? ? nil : address.to_yaml)
-              unless address.blank?
+              
+              unless address.blank? # <-- difference: attempts to geocode
                 address.geocode if self.class.geocoding_enabled
                 self.#{part_id}_latitude = address.latitude
                 self.#{part_id}_longitude = address.longitude
@@ -36,9 +37,9 @@ module GoingPostal
           class_eval <<-RUBY
             def #{part_id}
               attributes = super
+              attributes = YAML::load(attributes) if attributes.is_a?(String)
               return GoingPostal::Address.new if attributes.nil?
               
-              attributes = YAML::load(attributes) if attributes.is_a?(String)
               GoingPostal::Address.new(attributes)
             end
             
