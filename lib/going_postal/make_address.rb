a4bff14df37361e85dcda2bef78f1181625ed063
Bob Lail <bob.lail@cph.org>
2013-07-16 10:27:16 -0500

    [refactor] Refactored Geocoding options for addresses: detect automatically if latitude and longitude attributes are present; allow options to enable or disable server-side geocoding

--- lib/going_postal/make_address.rb
+++ lib/going_postal/make_address.rb
@@ -2,53 +2,36 @@ module GoingPostal
   module MakeAddress
     
     
-    attr_accessor :geocoding_enabled
-    
-    
     def make_address(*parts)
-      @geocoding_enabled = true
-      
       options = parts.extract_options!
-      geocode = options[:geocode]
       
       parts.each do |part_id|
-        if geocode
-          class_eval <<-RUBY
-            def #{part_id}
-              attributes = super
-              attributes = YAML::load(attributes) if attributes.is_a?(String)
-              return GoingPostal::Address.new if attributes.nil?
-              
-              GoingPostal::Address.new(attributes.merge(latitude: #{part_id}_latitude, longitude: #{part_id}_longitude)) # <-- difference: uses lat/long attrs
-            end
+        latitude_column = "#{part_id}_latitude"
+        longitude_column = "#{part_id}_longitude"
+        has_latlong = column_names.member?(latitude_column) and column_names.member?(longitude_column)
+        geocode = has_latlong && options[:geocode]
+        
+        class_eval <<-RUBY
+          def #{part_id}
+            attributes = super
+            attributes = YAML::load(attributes) if attributes.is_a?(String)
+            return GoingPostal::Address.new if attributes.nil?
             
-            def #{part_id}=(address)
-              address = GoingPostal::Address.new(address) if address.is_a?(Hash)
-              super(address.blank? ? nil : address.to_yaml)
-              
-              unless address.blank? # <-- difference: attempts to geocode
-                address.geocode if self.class.geocoding_enabled
-                self.#{part_id}_latitude = address.latitude
-                self.#{part_id}_longitude = address.longitude
-              end
-            end
-          RUBY
-        else
-          class_eval <<-RUBY
-            def #{part_id}
-              attributes = super
-              attributes = YAML::load(attributes) if attributes.is_a?(String)
-              return GoingPostal::Address.new if attributes.nil?
-              
-              GoingPostal::Address.new(attributes)
-            end
+            attributes = attributes.merge(latitude: #{latitude_column}, longitude: #{longitude_column}) if #{has_latlong}
+            GoingPostal::Address.new(attributes)
+          end
+          
+          def #{part_id}=(address)
+            address = GoingPostal::Address.new(address) if address.is_a?(Hash)
+            super(address.blank? ? nil : address.to_yaml)
             
-            def #{part_id}=(address)
-              address = GoingPostal::Address.new(address) if address.is_a?(Hash)
-              super(address.blank? ? nil : address.to_yaml)
+            if #{has_latlong} && !address.blank?
+              address.geocode if #{geocode}
+              self.#{latitude_column} = address.latitude
+              self.#{longitude_column} = address.longitude
             end
-          RUBY
-        end
+          end
+        RUBY
       end
     end
     
